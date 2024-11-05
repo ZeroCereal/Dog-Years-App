62e683733458997b74c6ec639921c2ad88ce3c09
ZeroCereal <93283947+ZeroCereal@users.noreply.github.com>
2024-11-05 07:56:31 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let myAge = 16;
+let myName = "Rocco";
+console.log(`My Age is ${myAge}`);
+// myAge equals the users age
+let earlyYears = myAge - 2;
+console.log(`2 years ago I was ${earlyYears}`);
+// This Variable is equal to the user age minus 2
+let laterYears = myAge + 2;
+console.log(`2 years for now I will be ${laterYears}`);
+earlyYears = earlyYears * 10.5;
+//times earlyYears by 10.5
+laterYears = laterYears * 4;
+// times laterYears by 4
+let dogYears = earlyYears + laterYears
+//earlyYears plus laterYears will equal the user age in dogYears
+console.log("My name is " + myName + ". I am " + myAge + " years old in human years which is " + dogYears + " years old in dog years.".toLowerCase());
